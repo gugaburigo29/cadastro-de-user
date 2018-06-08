9813c642691fbb788552e20ebce3d39ba542060a
Guga <guga.burigo29@outlook.com>
2018-06-08 11:12:03 -0300

    First

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass');
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function () {
+    return gulp.src('sass/btn.scss')
+        .pipe(concat('style.min.css'))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('public'));
+});
+gulp.task('watch', function () {
+    gulp.watch('sass', ['sass']);
+});
