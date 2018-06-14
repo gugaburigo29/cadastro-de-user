94fca64db6f80ab07db84e7c64fd1f7fdf981874
guga <guga.burigo29@outlook.com>
2018-06-14 20:49:47 -0300

    router

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    minify = require('gulp-minify');
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'js']);
 
 gulp.task('sass', function () {
-    return gulp.src('sass/style.scss')
+    return gulp.src('resources/sass/style.scss')
         .pipe(concat('style.min.css'))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('public'));
+});
+gulp.task('js', function () {
+    return gulp.src('resources/js/*.js')
+        .pipe(concat('script.js'))
+        .pipe(minify())
         .pipe(gulp.dest('public'));
 });
 gulp.task('watch', function () {
